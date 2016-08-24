22829f0495f3fc5f93e6471620b50fcfdc4f48b7
Michel Sousa <michelsousacarvalho@gmail.com>
2016-08-24 10:31:24 -0300

    Tab Resizer

--- lib/icon_resizer.ex
+++ lib/icon_resizer.ex
@@ -20,6 +20,14 @@ defmodule IconResizer do
     %{output_dir_name: "watch", size: 196},
   ]
 
+  @tab_sizes [
+      %{output_dir_name: "tab", size: 25, scale: "@1x"},
+      %{output_dir_name: "tab", size: 50, scale: "@2x"},
+      %{output_dir_name: "tab", size: 75, scale: "@3x"},
+  ]
+	
+
+
   defp resize_icon_file(icon) do
     icon_file = Path.expand(icon)
 
@@ -40,10 +48,48 @@ defmodule IconResizer do
 
     IO.puts "\nFinished with success!\n"
   end
+  
+  defp resize_tab_file(tab) do
+    icon_file = Path.expand(tab)
+
+    for %{output_dir_name: output_dir_name, size: size, scale: scale} <- @tab_sizes do
+      output_dir = [Path.dirname(icon_file), output_dir_name] |> Path.join
+
+      output_dir |> File.mkdir_p
+
+      resized_image = [output_dir, "#{Path.basename(icon_file, ".png")}_#{size}_#{scale}.png"] |> Path.join
+
+      open(tab)
+      |> copy
+      |> resize("#{size}x#{size}!")
+      |> save(resized_image)
+
+      IO.puts "#{resized_image} generated!"
+    end
+
+    IO.puts "\nFinished with success!\n"
+  end
+  
+  
 
   def main(args) do
-    {[icon: icon], _, _} = OptionParser.parse(args)
+    # {[icon: icon], _, _} = OptionParser.parse(args)
+    # resize_icon_file(icon)
+	
+	{options, _, _}  = OptionParser.parse(args)
 
-    resize_icon_file(icon)
+	result = hd(options) |> elem(0) |> to_string
+		
+	cond do
+		result === "tab" ->
+			resize_tab_file(hd(options) |> elem(1))
+		result === "t" ->
+			resize_tab_file(hd(options) |> elem(1))
+		result === "icon" ->
+			resize_icon_file(hd(options) |> elem(1))
+		result === "i" ->
+			resize_icon_file(hd(options) |> elem(1))
+	end
+	
   end
 end
